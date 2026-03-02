0b5431598069462c9a6d606739b83f33c6a6e9f4
Delmat237 <azangueleonel9@gmail.com>
2026-03-02 16:25:04 +0100

    feat: enable importing accounting plans from CSV and XLSX files via a new endpoint and dedicated service.

--- src/main/java/com/yowyob/erp/config/auth/AuthValidationResponse.java
+++ src/main/java/com/yowyob/erp/config/auth/AuthValidationResponse.java
@@ -1,12 +1,16 @@
 package com.yowyob.erp.config.auth;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // DTO for validation response
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthValidationResponse {
     private boolean valid;
     private String userId;
